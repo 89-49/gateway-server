5ee8bb3aa70e947a5180cc874c7c0008c075b8b1
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-05-04 00:08:29 +0900

    refactor: 게이트웨이 필터 구현 방식 변경
    - 구현의 복잡성을 낮추면서 .yaml 파일 시반 라우팅 설정 적용을 목적으로 수행
    - 기존 HandlerFilterFunction 기반 인증 필터로 요청 발송 시 라우팅 설정이 제대로 적용되지 않는 문제 개선
    - HandlerFilterFunction 대신 OncePerRequestFilter를 기반으로 하여 게이트웨이 인증 필터가 동작하도록 수정

---
+++ src/main/java/org/pgsg/gateway/filter/HttpRequestHeaderWrapper.java
@@ -0,0 +1,69 @@
+package org.pgsg.gateway.filter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class HttpRequestHeaderWrapper extends HttpServletRequestWrapper {
+
+	private static final String FORBIDDEN_HEADER_PREFIX = "x-user-";
+
+	private final Map<String, String> customHeaders = new HashMap<>();
+
+	public HttpRequestHeaderWrapper(HttpServletRequest request) {
+		super(request);
+	}
+
+	public void putHeader(String name, String value) {
+		customHeaders.put(name.toLowerCase(), value);
+	}
+
+	// x-user- 로 시작하는 헤더 일괄 제거 (스푸핑 방지)
+	public void removeHeaders(String prefix) {
+		Collections.list(super.getHeaderNames()).stream()
+				.filter(name -> name.toLowerCase().startsWith(prefix.toLowerCase()))
+				.forEach(customHeaders::remove);
+	}
+
+	@Override
+	public String getHeader(String name) {
+		String lowerName = name.toLowerCase();
+		if (customHeaders.containsKey(lowerName)) {
+			return customHeaders.get(lowerName);
+		}
+		if (lowerName.startsWith(FORBIDDEN_HEADER_PREFIX)) {
+			return null;
+		}
+		return super.getHeader(name);
+	}
+
+	@Override
+	public Enumeration<String> getHeaders(String name) {
+		String lowerName = name.toLowerCase();
+		String value = customHeaders.get(lowerName);
+
+		if (customHeaders.containsKey(lowerName)) {
+			// 리스트의 길이가 1인 경우에도 호환
+			return Collections.enumeration(Collections.singletonList(value));
+		}
+		if (lowerName.startsWith(FORBIDDEN_HEADER_PREFIX)) {
+			return Collections.emptyEnumeration();
+		}
+		return super.getHeaders(name);
+	}
+
+	@Override
+	public Enumeration<String> getHeaderNames() {
+		Set<String> names = Collections.list(super.getHeaderNames()).stream()
+				.map(String::toLowerCase)
+				.filter(headerName ->
+						!customHeaders.containsKey(headerName) && 			// 직접 추가한 요청 헤더가 아니면서
+						!headerName.startsWith(FORBIDDEN_HEADER_PREFIX)		//금지된 접두사로 시작하는 헤더가 아님
+				)
+				.collect(Collectors.toCollection(LinkedHashSet::new));
+		names.addAll(customHeaders.keySet());
+		return Collections.enumeration(names);
+	}
+}
