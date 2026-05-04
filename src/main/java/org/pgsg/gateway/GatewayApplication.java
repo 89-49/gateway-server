914d4dea3b07aaff29172a97cd642fcd2f172a10
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-05-04 09:15:19 +0900

    chore: build.gradle 의존성 추가 및 FeignClient 활성화
    - user-service와의 동기식 통신을 통해 토큰 블랙리스트 검증을 수행하기 위한 FeignClient 추가
    - traceId를 할당 작업용 라이브러리 추가
    - GatewayApplication 내 @EnableFeignClients 추가

--- src/main/java/org/pgsg/gateway/GatewayApplication.java
+++ src/main/java/org/pgsg/gateway/GatewayApplication.java
@@ -4,9 +4,11 @@ import org.pgsg.config.AppCtx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @ImportAutoConfiguration(exclude = AppCtx.class)
+@EnableFeignClients
 public class GatewayApplication {
 
 	public static void main(String[] args) {
